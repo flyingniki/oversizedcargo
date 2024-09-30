358fd9d37da891affe3696bf2933287051ed7b70
Petr Nikitin <petr.niki.777@gmail.com>
2024-09-30 11:48:57 +0300

    оживлен бургер

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,16 +1,15 @@
-// const dropDowns = document.querySelectorAll(".dropdown");
-// // console.log(dropDowns);
+const nav = document.querySelector(".nav");
+const navToggle = document.querySelector(".nav__toggle");
+const links = document.querySelectorAll(".nav__link");
 
-// dropDowns.forEach((dropDown) => {
-//   dropDown.addEventListener("click", (e) => {
-//     // console.log("click");
-//     // let children = Array.from(dropDown.children);
-//     // // console.log(children);
-//     // children.forEach((child) => {
-//     //   if (child.classList.contains("nav__submenu")) {
-//     console.log(e.target);
-//     //     child.classList.toggle = "visually-hidden";
-//     //   }
-//     // });
-//   });
-// });
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--closed");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.classList.toggle("active");
+    let submenu = link.nextElementSibling;
+    submenu.classList.toggle("visually-hidden");
+  });
+});
