2bac4366239d0ea885158133993513ffe6fb3b14
Petr Nikitin <petr.niki.777@gmail.com>
2024-10-07 16:20:39 +0300

    секция work

--- source/js/slider.js
+++ source/js/slider.js
@@ -29,9 +29,22 @@ $(document).ready(function () {
     centerMode: false,
   });
 
+  if ($(window).width() < "1440") {
+    $(".work__carousel").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: false,
+      centerMode: true,
+    });
+  }
+
   const slickArrows = [
     $(".services__carousel .slick-arrow"),
     $(".car .slick-arrow"),
+    $(".work__carousel .slick-arrow"),
   ];
 
   slickArrows.forEach((arrow) => {
